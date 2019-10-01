4607d95cbf39e2d2c91d9ecdbecf7b3a0f759159
Hope Giometti <hopegiometti@gmail.com>
2019-10-01 19:11:44 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -163,11 +163,11 @@ def player_numbers(team_name)
     if team[:team_name] == team_name
       team.each do |attribute, data|
         if attribute == :players
-          return ["#{data[:players][:number]}",
-          "#{data[players][:number]}",
-          "#{data[:players][:number]}",
-          "#{data[:players][:number]}",
-          "#{data[:players][:number]}"
+          return [data[:players][:number],
+          data[:players][:number],
+          data[:players][:number],
+          data[:players][:number],
+          data[:players][:number]
           ]
         end
       end
