420055ed3bb50b3fc0b892d938e7d9be582657d1
Hope Giometti <hopegiometti@gmail.com>
2019-10-01 19:01:28 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -167,7 +167,7 @@ def player_numbers(team_name)
           "#{attribute[:players][:number]}",
           "#{attribute[:players][:number]}",
           "#{attribute[:players][:number]}",
-          "#{attribute[:players][:number]}" 
+          "#{attribute[:players][:number]}"
         ]
         end
       end
